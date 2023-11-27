ade59d1c194296ce95380a32f75e36a9b91d0b77
nascimentoannajulia <nascimentoannajulia253@gmail.com>
2023-11-27 20:58:45 -0300

    Aula 1 a 5

--- Seção01/primeiro-projeto/src/app/app.component.ts
+++ Seção01/primeiro-projeto/src/app/app.component.ts
@@ -4,11 +4,9 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeiro-projeto';
+  title = 'Olá Mundo!';
 }
