238c1486b9d0cfc9cc3f4e1ce7b5a9ebfff48448
nascimentoannajulia <nascimentoannajulia253@gmail.com>
2023-12-05 19:09:02 -0300

    Aula 3 a 10

---
+++ primeiro-projeto/src/app/cursos/cursos/cursos-detalhe/cursos-detalhe.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cursos-detalhe',
+  standalone: true,
+  imports: [],
+  templateUrl: './cursos-detalhe.component.html',
+  styleUrl: './cursos-detalhe.component.css'
+})
+export class CursosDetalheComponent {
+
+}
