2fbc79e4d0946693b77aaf6faec269610759ba89
nascimentoannajulia <nascimentoannajulia253@gmail.com>
2023-12-13 19:01:50 -0300

    Aula 31 a 35

---
+++ 04_diretivas/src/app/operador-elvis/operador-elvis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-operador-elvis',
+  standalone: true,
+  imports: [],
+  templateUrl: './operador-elvis.component.html',
+  styleUrl: './operador-elvis.component.css'
+})
+export class OperadorElvisComponent {
+
+}
